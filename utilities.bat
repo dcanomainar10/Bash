8441ca18598e118f68542e0f9c21decdf5891662
dcanomainar <danielcanotapia@gmail.com>
2022-11-11 13:58:40 +0100

    Minor change

--- utilities.bat
+++ utilities.bat
@@ -1,26 +1,57 @@
-# utils
+@ECHO OFF 
+
+ECHO ==========================
+ECHO INSTALLING UTILITIES
+ECHO ==========================
+
 scoop install 7zip sumatrapdf curl sudo openssh coreutils grep sed less
 
-# programming utils
+ECHO ==========================
+ECHO PROGRAMMING UTILITIES
+ECHO ==========================
+
 scoop install postman git
 
-# programming languages
+ECHO ==========================
+ECHO INSTALLING PROGRAMMING LANGUAGES
+ECHO ==========================
+
 scoop install python nodejs
 
-# ides
+ECHO ==========================
+ECHO INSTALLING IDES
+ECHO ==========================
+
 scoop install vscode typora rstudio sql-server-management-studio-np notepadplusplus
 
-# devops
+ECHO ==========================
+ECHO INSTALLING DEVOPS
+ECHO ==========================
+
 scoop install docker
 
-# browsers
+ECHO ==========================
+ECHO INSTALLING BROWSERS
+ECHO ==========================
+
 scoop install firefox
 
-# communications
+ECHO ==========================
+ECHO INSTALLING COMMUNICATIONS
+ECHO ==========================
+
 scoop install discord skype
 
-# entertainment 
+ECHO ==========================
+ECHO INSTALLING ENTERTAINMENT
+ECHO ==========================
+
 scoop install vlc spytify
 
-# design
+ECHO ==========================
+ECHO INSTALLING DESIGN
+ECHO ==========================
+
 scoop install inkscape gimp
+
+PAUSE
