162ccf94ea936c832e38a8fef70155ddb095ba3f
dcanomainar10 <dcanomainar@gmail.com>
2022-11-15 21:49:33 +0100

    Log saved into file

--- install.bat
+++ install.bat
@@ -1,9 +1,11 @@
+@echo off
+
+call :sub >%~dp0output.log
+exit /b
+
+:sub
 call pushd "%~dp0
-
 Powershell.exe -executionpolicy remotesigned -File 1.install_chocolatey.ps1
-
 Powershell.exe -executionpolicy remotesigned -File 2.install_scoop.ps1 -RunAsAdmin
-
 call 3.install_apps.bat
-
-set /p=Hit ENTER to continue...
+exit /b
